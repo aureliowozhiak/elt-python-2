7c517f935f15ff0bdc556f3ef7f333b3565081cf
Marcos Aurelio Wozhiak Junior de Oliveira <aureliowozhiak@gmail.com>
2024-03-08 14:01:33 -0300

    add argv

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ from methods.extract import Extract
 from methods.transform import Transform
 from methods.load import Load
 from methods.tags import tags
+import sys
 
-url = "https://pt.wikipedia.org/wiki/Python"
+def run(url):
+    extract = Extract(url)
+    load = Load()
 
-extract = Extract(url)
-load = Load()
+    file_name = "_".join(url.split("/")[2::]).replace(".", "_")
 
-i = 0
-for table in extract.get_tags(tags["table"]):
-    transform = Transform(table)
-    df = transform.transform()
-    if df.is_empty():
-        continue
+    i = 0
+    for table in extract.get_tags(tags["table"]):
+        transform = Transform(table)
+        df = transform.transform()
+        if df.is_empty():
+            continue
 
-    load.save(df, f"{i}.csv")
-    i += 1
+        load.save(df, f"{file_name}_{i}.csv")
+        i += 1
+
+for url in sys.argv[1:]:
+    run(url)
